b6f7486aeed6edb9ec4931aca6e304f306ce30be
Daniele Tabanella <daniele.tabanella@gmail.com>
2020-06-05 22:00:15 +0200

    add some sprites

---
+++ sprites/car.asm
@@ -0,0 +1,26 @@
+;---Graphics Data from PlayerPal 2600---
+
+Frame0
+        .byte #%01000010;$00
+        .byte #%11111110;$00
+        .byte #%11111111;$00
+        .byte #%01111111;$00
+        .byte #%01111111;$94
+        .byte #%00111111;$00
+        .byte #%00011101;$00
+        .byte #%00001101;$00
+;---End Graphics Data---
+
+
+;---Color Data from PlayerPal 2600---
+
+ColorFrame0
+        .byte #$00;
+        .byte #$00;
+        .byte #$00;
+        .byte #$00;
+        .byte #$94;
+        .byte #$00;
+        .byte #$00;
+        .byte #$00;
+;---End Color Data---
---
+++ sprites/guy.asm
@@ -0,0 +1,188 @@
+;---Graphics Data from PlayerPal 2600---
+
+Frame0
+        .byte #%00010100;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00101010;$0E
+        .byte #%00111110;$0E
+        .byte #%00001000;$3E
+        .byte #%00011000;$00
+Frame1
+        .byte #%00000100;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00101010;$0E
+        .byte #%00111110;$0E
+        .byte #%00001000;$3E
+        .byte #%00011000;$00
+Frame2
+        .byte #%00010000;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00101010;$0E
+        .byte #%00111110;$0E
+        .byte #%00001000;$3E
+        .byte #%00001100;$00
+Frame3
+        .byte #%00110100;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00001010;$0E
+        .byte #%00111110;$0E
+        .byte #%00101000;$3E
+        .byte #%00011000;$00
+Frame4
+        .byte #%00010100;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00101010;$0E
+        .byte #%00111110;$0E
+        .byte #%00001000;$3E
+        .byte #%00011000;$00
+Frame5
+        .byte #%00110100;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00101010;$0E
+        .byte #%00111110;$0E
+        .byte #%00001000;$3E
+        .byte #%00001000;$00
+Frame6
+        .byte #%00110100;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00101000;$0E
+        .byte #%00111110;$0E
+        .byte #%00001010;$3E
+        .byte #%00001100;$00
+Frame7
+        .byte #%00010110;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00101000;$0E
+        .byte #%00111110;$0E
+        .byte #%00001000;$3E
+        .byte #%00001100;$00
+Frame8
+        .byte #%00110110;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00001000;$0E
+        .byte #%00111110;$0E
+        .byte #%00101010;$3E
+        .byte #%00001100;$00
+Frame9
+        .byte #%00110110;$34
+        .byte #%00010100;$70
+        .byte #%00011100;$70
+        .byte #%00011100;$0E
+        .byte #%00001000;$0E
+        .byte #%00111110;$0E
+        .byte #%00101010;$3E
+        .byte #%00001100;$00
+;---End Graphics Data---
+
+
+;---Color Data from PlayerPal 2600---
+
+ColorFrame0
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame1
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame2
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame3
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame4
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame5
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame6
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame7
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame8
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+ColorFrame9
+        .byte #$34;
+        .byte #$70;
+        .byte #$70;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$0E;
+        .byte #$3E;
+        .byte #$00;
+;---End Color Data---
